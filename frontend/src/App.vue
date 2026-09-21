b5991b86bc523f80cc0c2f8d115cc228b8592c74
sleepkingking <3539883610@qq.com>
2026-09-21 22:14:49 +0800

    搭建项目环境并创建前端标准Vue工程
    总之，以后前端环境的配置就是按照这一套来

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,4 +1,6 @@
-<script setup></script>
+<script setup>
+import { Edit } from '@element-plus/icons-vue'
+</script>
 
 <template>
   <h1>You did it!</h1>
@@ -6,6 +8,10 @@
     Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
     documentation
   </p>
+  <el-button type="primary">按钮</el-button>
+  <div>
+    <el-icon size="small"><Edit /></el-icon>
+  </div>
 </template>
 
 <style scoped></style>
